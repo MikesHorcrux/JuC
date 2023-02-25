ba37c0cce03e52b16c08d562dc4bca86149fc06f
justBearded <vanamburg.mv@gmail.com>
2023-02-25 14:45:35 -0600

    Initial Commit

---
+++ JuC/JuCApp.swift
@@ -0,0 +1,17 @@
+//
+//  JuCApp.swift
+//  JuC
+//
+//  Created by Mike on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct JuCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
