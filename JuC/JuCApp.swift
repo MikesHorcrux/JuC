1f3f4e9efb309e1addcdb2a16e5c622cdbafd188
justBearded <vanamburg.mv@gmail.com>
2023-04-03 20:07:39 -0500

    init sentry

--- JuC/JuCApp.swift
+++ JuC/JuCApp.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import FirebaseCore
 import KeyboardObserving
+import Sentry
 
 @main
 struct JuCApp: App {
@@ -16,20 +17,30 @@ struct JuCApp: App {
     
     init() {
         FirebaseApp.configure()
+        
+        SentrySDK.start { options in
+            options.dsn = "https://28b0a37535c74edf9ed4850daee65897@o1189287.ingest.sentry.io/4504952708726784"
+            options.debug = true // Enabled debug when first installing is always helpful
+            
+            // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+            // We recommend adjusting this value in production.
+            options.tracesSampleRate = 1.0
+            
+        }
     }
-    
-    @ViewBuilder
-    var body: some Scene {
-        WindowGroup {
-            if authManager.user == nil {
-                AuthView()
-                    .environmentObject(authManager)
+        
+        @ViewBuilder
+        var body: some Scene {
+            WindowGroup {
+                if authManager.user == nil {
+                    AuthView()
+                        .environmentObject(authManager)
                     //.environmentObject(Keyboard()) // Add the Keyboard environment object here.
-            } else {
-                ContentView()
-                    .environmentObject(authManager)
-                    .environmentObject(Keyboard()) // Add the Keyboard environment object here.
+                } else {
+                    ContentView()
+                        .environmentObject(authManager)
+                        .environmentObject(Keyboard()) // Add the Keyboard environment object here.
+                }
             }
         }
     }
-}
